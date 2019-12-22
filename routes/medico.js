3d375a3808c694429403470c87542eb831ecce8a
jajtuco <jajtome@gmail.com>
2019-12-22 10:36:24 -0300

    Correcciones en hospital.js y medico.js

--- routes/medico.js
+++ routes/medico.js
@@ -47,6 +47,41 @@ app.get('/', (req, res, next) => {
 
 });
 
+// ===================================
+// Obtener médico
+// ===================================
+app.get('/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    Medico.findById( id )
+    .populate( 'usuario', 'nombre email img' )
+    .populate( 'hospital' )
+    .exec( (err, medico) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar medico',
+                errors: err
+            });
+        }
+
+        if (!medico) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El medico con el id ' + id + ' no existe',
+                errors: { message: 'No existe un medico con ese ID' }
+            });
+        }
+
+        return res.status(200).json({
+            ok: true,
+            medico: medico
+        });
+
+    });
+});
 
 // ===================================
 // Actualizar medicos
@@ -93,7 +128,7 @@ app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
 
             res.status(200).json({
                 ok: true,
-                body: medicoGuardado
+                medico: medicoGuardado
             });
 
         });
@@ -131,7 +166,7 @@ app.post('/', mdAutenticacion.verificaToken, (req, res) => {
 
         res.status(201).json({
             ok: true,
-            body: medicoGuardado
+            medico: medicoGuardado
         });
 
     });
