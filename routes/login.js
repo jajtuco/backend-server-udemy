94d0402118f271b524a0e99b5f5089e80456476d
Jorge Tomé <jajtome@gmail.com>
2019-12-26 18:08:16 -0300

    Fin seccion 18

--- routes/login.js
+++ routes/login.js
@@ -12,6 +12,24 @@ var CLIENT_ID = require('../config/config').CLIENT_ID;
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(CLIENT_ID);
 
+var mdAutenticacion = require('../middelwares/autenticacion');
+
+
+// ===================================
+// Renovar token
+// ===================================
+app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res ) => {
+
+
+    var token = jwt.sign({ usuario: req.usuario }, SEED, { expiresIn: 14400 }); // +4hs 
+
+    return res.status(200).json({
+        ok: true,
+        token: token
+    });
+
+});
+
 
 // ===================================
 // Autenticacion de google
